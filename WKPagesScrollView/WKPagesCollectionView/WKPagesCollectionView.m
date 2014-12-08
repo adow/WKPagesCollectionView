f2432d869f4a07fe5b88333b05068bbd0f2ca748
nick-iqnect <nick@iqnect.org>
2014-12-08 15:09:29 +0800

    For fix a crashing issue

--- WKPagesScrollView/WKPagesCollectionView/WKPagesCollectionView.m
+++ WKPagesScrollView/WKPagesCollectionView/WKPagesCollectionView.m
@@ -151,7 +151,6 @@ CGFloat const TOP_OFFSCREEN_MARGIN = 120;
     if (_isHighLight){
         return;
     }
-    [self scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionCenteredVertically animated:NO];
     double delayInSeconds = 0.01f;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
